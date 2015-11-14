4b0443c0f9247fb196f93ada4edfc4daa85b00e9
Stefan Mehnert <stefanmehnert@gmail.com>
2015-11-14 19:47:13 +0100

    Passes Test 4, leap year is introduced every 400 years.

--- leap.js
+++ leap.js
@@ -1,6 +1,6 @@
 var isLeapYear = function(year) {
 	
-	if(year % 4 == 0 && year % 100 != 0) {
+	if(year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0 ) ) {
 		return true;
 	}
 	
--- leap.spec.js
+++ leap.spec.js
@@ -14,7 +14,7 @@ describe('Leap year', function() {
     expect(isLeapYear(1900)).toBe(false);
   });
 
-  xit('is reintroduced every 400 years to adjust another day', function() {
+  it('is reintroduced every 400 years to adjust another day', function() {
     expect(isLeapYear(2000)).toBe(true);
   });
 
